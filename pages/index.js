9fd06b96da66fb100c4741069d61c3accead9ae1
Jefferson Roberto Bublitz Jr <bublitzjr@gmail.com>
2021-07-13 23:45:24 -0300

    Ajusts colors

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={styles.h1}>
           Coming soon !
         </h1>
       </main>
