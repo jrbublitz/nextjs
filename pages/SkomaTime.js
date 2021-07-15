cf2538a943f2cb796c07ff58070b8748fee710c9
Jefferson Roberto Bublitz Jr <bublitzjr@gmail.com>
2021-07-15 20:27:01 -0300

    Typing effect

--- pages/SkomaTime.js
+++ pages/SkomaTime.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css'
+import Typical from 'react-Typical'
 
 const skommaTime = () => {
   return (
@@ -6,7 +7,11 @@ const skommaTime = () => {
 
       <main className={styles.main}>
         <blockquote className={styles.blockquote}>
-          <h5>HELLO OUTSIDERRR, LET'S DRINK SKOOOOMAAAA !!!</h5>
+          <p>          
+            <Typical
+              steps={["HELLO OUTSIDERRR, LET'S DRINK SKOOOOMAAAA !!!"]}
+            />          
+          </p>
         </blockquote>
         <a className={styles.emoji}>🍺🦁</a>
       </main>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import styles from '../styles/Home.module.css'
+import Typical from 'react-Typical'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <blockquote className={styles.blockquote}>
-          <h5>Be patient outsider. We need to wait for the scout group to come back for more information, they will be back soon. Here take a beer.</h5>
-        </blockquote>
+export default function Home() {  
+  return (      
+    <div className={styles.container}> 
+      <main className={styles.main}>      
+        <blockquote className={styles.blockquote}>          
+          <p>
+            <Typical
+            steps={["Be patient outsider. We need to wait for the scout group to come back for more information, they will be back soon. Here take a beer."]}
+            />            
+          </p>          
+        </blockquote>        
         <a className={styles.emoji}>🍺🧝🏻‍♂</a>
       </main>
     </div>
