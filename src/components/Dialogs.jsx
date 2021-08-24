2adea2629b2116e509b2c8270661979d32031a5e
Jefferson Roberto Bublitz Jr <bublitzjr@gmail.com>
2021-08-23 23:23:18 -0300

    css rewrite

--- src/components/Dialogs.jsx
+++ src/components/Dialogs.jsx
@@ -5,11 +5,11 @@ import Head from 'next/head';
 
 export default function Dialogs(props) {   
 
- var logo = '< JEFF />'
+ var logo = '< Cap. 01 />'
 
   return (   
     <html lang="pt-br">
-      <div className={styles.container} lang={'pt-br'}>
+      <body className={styles.container} lang={'pt-br'}>
         <Head>          
           <title>Welcome Adventurer 🍺</title>                   
           <link rel="icon" href="images/swords.png"/>;
@@ -22,17 +22,18 @@ export default function Dialogs(props) {
         <main className={styles.main}>         
           <blockquote className={styles.blockquote}>          
             <TypeScript dialog={props.dialog} />            
-          </blockquote>                  
+          </blockquote> 
+          
+          <div className={styles.emoji}>
+            <a>{props.person}</a>
+         </div>              
         </main>
-        <div>
-          <a className={styles.emoji}>{props.person}</a>
-        </div>
         
         <footer className={styles.footer}>        
           <a href="https://github.com/jrbublitz"><img src="images/github.png" alt="github" className={styles.github} /></a>
           <a href="https://www.linkedin.com/in/jefferson-roberto-bublitz-junior-338193101/"><img src="images/linkedin.png" alt="linkedin" className={styles.link} /></a>        
         </footer>
-      </div>    
+      </body>    
     </html>        
   ) 
 }
